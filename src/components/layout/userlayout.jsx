09dcb13b8e618c4e5646353f65213735b42a75be
greatkendo <okwuken1000@gmail.com>
2025-04-10 20:47:41 +0100

    update

--- src/components/layout/userlayout.jsx
+++ src/components/layout/userlayout.jsx
@@ -1,4 +1,8 @@
+import Footer from "../common/footer";
 import Header from "../common/header";
+import GenderCollectionSection from "../products/gendercollectionsection";
+import NewArrivals from "../products/newarrivals";
+import Hero from "./hero";
 
 function UserLayout () {
  return (
@@ -6,7 +10,13 @@ function UserLayout () {
   {/* Header */}
   <Header />
   {/* Main content */}
+  <main className="container mx-auto py-4 px-6">
+   <Hero />
+   <GenderCollectionSection />
+   <NewArrivals />
+  </main>
   {/* Footer */}
+  <Footer />
   </>
  );
 };
